dabf880420fafc503d5fec9602af7535d8c1a380
Luke Mendelman-Haenn <glukemh@gmail.com>
2023-11-26 13:43:07 -0700

    web rtc signaling

---
+++ functions/api/room/signal.js
@@ -0,0 +1,117 @@
+/**
+ * @typedef {Object} RTCSessionDescriptionInit
+ * @property {"offer" | "answer"} type
+ * @property {string} sdp
+ */
+/**
+ * @typedef {Object} RTCIceCandidateInit
+ * @property {string} candidate
+ * @property {string} sdpMid
+ * @property {number} sdpMLineIndex
+ * @property {string} usernameFragment
+ */
+
+/**
+ * @typedef {{ rooms: any }} Env
+ */
+
+/**
+ * Room object stored in KV
+ * @typedef {{ offer: RTCSessionDescriptionInit, offerClient: WebSocket, answer: RTCSessionDescriptionInit, answerClient: WebSocket, iceCandidates: RTCIceCandidateInit[], createdAt: number }[]} Room
+ */
+
+/**
+ * @typedef {WebSocket & { accept: () => void }} WebSocketWithAccept
+ */
+
+/**
+ * @param {Parameters<PagesFunction<Env>>} args
+ * @returns {Promise<Response>}
+ */
+export async function onRequestPost(...args) {
+	const [{ request, env }] = args;
+	const url = new URL(request.url);
+	const roomName = url.searchParams.get("roomName");
+	if (!roomName || roomName.length > 32) {
+		return new Response("Expected roomName (shorter than 32 characters)", {
+			status: 400,
+		});
+	}
+	const { rooms } = env;
+	try {
+		const room = rooms.get(rooms.idFromName(roomName));
+		return room.fetch(request);
+	} catch (error) {
+		console.error(error);
+		return new Response("Something went wrong", { status: 500 });
+	}
+}
+
+/**
+ * @typedef {Object} OfferAnswers
+ * @property {string} uid
+ * @property {RTCSessionDescriptionInit} offer
+ * @property {Map<string, RTCSessionDescriptionInit>} answer Map from uid to answer
+ */
+
+export class RoomConnection {
+	/**
+	 * @type {Map<string, WebSocket>}
+	 */
+	sessions = new Map();
+	/**
+	 * Durable Object constructor
+	 * @param {DurableObjectState} _state
+	 * @param {Env} _env
+	 */
+	constructor(_state, _env) {}
+
+	/**
+	 * Durable Object fetch handler
+	 * @param {Request} request
+	 */
+	async fetch(request) {
+		const upgradeHeader = request.headers.get("Upgrade");
+		if (upgradeHeader?.toLowerCase() !== "websocket") {
+			return new Response("Expected websocket", { status: 426 });
+		}
+		const url = new URL(request.url);
+		const uid = url.searchParams.get("uid");
+		if (!uid) {
+			return new Response("Expected uid", { status: 400 });
+		}
+		let [client, server] = Object.values(new WebSocketPair());
+		/** @type {WebSocketWithAccept} */ (server).accept();
+		const existingClient = this.sessions.get(uid);
+		if (existingClient) {
+			existingClient.close();
+		}
+
+		server.addEventListener("message", async (event) => {
+			if (typeof event.data !== "string") {
+				return;
+			}
+			const message = JSON.parse(event.data);
+			if (typeof message.to === "string") {
+				const client = this.sessions.get(message.uid);
+				if (!client) return;
+				client.send(JSON.stringify(message));
+			} else if (Array.isArray(message.to)) {
+				for (const uid of message.to) {
+					if (typeof uid !== "string") continue;
+					const client = this.sessions.get(uid);
+					if (!client) continue;
+					client.send(JSON.stringify(message));
+				}
+			}
+		});
+
+		this.sessions.set(uid, client);
+		client.send(JSON.stringify({ users: [...this.sessions.keys()] }));
+		return new Response(null, {
+			status: 101,
+			// @ts-ignore
+			webSocket: client,
+		});
+	}
+}
--- public/assets/room.js
+++ public/assets/room.js
@@ -1,56 +1,195 @@
-const servers = {
-	iceServers: [
-		{
-			urls: ["stun:stun.l.google.com:19302", "stun:stun1.l.google.com:19302"],
-		},
-	],
-};
+let UID = sessionStorage.getItem("uid") || "";
+if (!UID) {
+	UID = Math.random().toString(36).slice(2);
+	sessionStorage.setItem("uid", UID);
+}
+
+class RoomPeerConnection {
+	static servers = {
+		iceServers: [
+			{
+				urls: ["stun:stun.l.google.com:19302", "stun:stun1.l.google.com:19302"],
+			},
+		],
+	};
+	/**
+	 * @type {RTCDataChannel | null}
+	 */
+	dataChannel = null;
+	/**
+	 * Promise that resolves when the websocket is connected
+	 * @type {Promise<void>}
+	 */
+	wsConnected;
+	/**
+	 * Id of the other user to connect to
+	 * @type {string}
+	 */
+	user;
+	/**
+	 * WebSocket used to signal
+	 * @param {WebSocket} ws
+	 * @param {string} user
+	 * @param {RTCSessionDescriptionInit} [offer]
+	 */
+	constructor(user, ws, offer) {
+		this.user = user;
+		this.ws = ws;
+		this.wsConnected = new Promise((resolve, reject) => {
+			switch (ws.readyState) {
+				case WebSocket.CONNECTING:
+					ws.addEventListener("open", () => resolve());
+					break;
+				case WebSocket.OPEN:
+					resolve();
+					break;
+				default:
+					reject();
+					break;
+			}
+		});
+		this.peerConnection = new RTCPeerConnection(RoomPeerConnection.servers);
+		this.peerConnection.addEventListener("icecandidate", (e) => {
+			if (!e.candidate) return;
+			this.signalOverWs(e.candidate);
+		});
+		this.peerConnection.addEventListener("datachannel", (e) => {
+			console.log("datachannel", e);
+			const { channel } = e;
+			channel.addEventListener("open", () => {
+				console.log("datachannel open");
+				this.dataChannel = channel;
+			});
+			channel.addEventListener("message", (e) => {
+				console.log("datachannel message", e);
+			});
+			channel.addEventListener("close", () => {
+				console.log("datachannel close");
+				this.dataChannel = null;
+			});
+		});
+
+		this.ws.addEventListener("message", async (e) => {
+			if (typeof e.data !== "string") return;
+			const data = JSON.parse(e.data);
+			if (data.from !== this.user) return;
+			const { message } = data;
+			switch (message.type) {
+				case "offer":
+					this.answer(/** @type {RTCSessionDescriptionInit} */ (message));
+					break;
+				case "answer":
+					this.peerConnection.setRemoteDescription(message);
+					break;
+				case "candidate":
+					this.peerConnection.addIceCandidate(message);
+					break;
+			}
+		});
+		if (offer) {
+			this.answer(offer);
+		} else {
+			this.offer();
+		}
+	}
+
+	/**
+	 * Answer to an offer
+	 * @param {RTCSessionDescriptionInit} offer
+	 * @returns {Promise<void>}
+	 */
+	async answer(offer) {
+		this.peerConnection.setRemoteDescription(offer);
+		const answer = await this.peerConnection.createAnswer();
+		this.peerConnection.setLocalDescription(answer);
+		this.signalOverWs(answer);
+	}
+
+	/**
+	 * Send an offer to the other user
+	 * @returns {Promise<void>}
+	 */
+	async offer() {
+		const offer = await this.peerConnection.createOffer();
+		this.peerConnection.setLocalDescription(offer);
+		this.signalOverWs(offer);
+	}
+
+	/**
+	 * Send messages over websocket primarily for signaling
+	 * @param {any} message
+	 */
+	async signalOverWs(message) {
+		await this.wsConnected;
+		this.ws.send(JSON.stringify({ from: UID, to: this.user, message }));
+	}
+}
 
 class Room {
+	/**
+	 * Peer connections
+	 * @type {Map<string, RoomPeerConnection>}
+	 */
+	peers = new Map();
+	/**
+	 * Websocket used for signaling
+	 * @type {WebSocket}
+	 */
+	ws;
 	/**
 	 * Constructor takes in a room id and creates a peer connection
 	 * @param {string} id
 	 */
 	constructor(id) {
 		this.id = id;
-		this.peerConnection = new RTCPeerConnection(servers);
-	}
-
-	async createOffer() {
-		const offer = await this.peerConnection.createOffer();
-		await this.peerConnection.setLocalDescription(offer);
-		return offer;
-	}
-
-	async createAnswer() {
-		const answer = await this.peerConnection.createAnswer();
-		await this.peerConnection.setLocalDescription(answer);
-		return answer;
+		const url = new URL("/api/room/signal", location.origin);
+		url.searchParams.set("roomId", this.id);
+		url.searchParams.set("uid", UID);
+		this.ws = new WebSocket(url);
+		this.ws.addEventListener("open", (e) => {
+			console.log("Connected to room");
+		});
+		this.ws.addEventListener("close", () => {
+			console.log("Disconnected from room");
+		});
+		this.ws.addEventListener("error", () => {
+			console.error("Error connecting to room");
+		});
+		this.ws.addEventListener("message", (e) => this.handleWSMessage(e));
 	}
 
 	/**
-	 * Sets the remote description for the peer connection.
-	 * @param {RTCSessionDescriptionInit} remoteDescription - The remote description to set.
-	 * @returns {Promise<void>} - A promise that resolves when the remote description is set.
+	 * Handle websocket messages
+	 * @param {MessageEvent<any>} e
 	 */
-	async setRemoteDescription(remoteDescription) {
-		await this.peerConnection.setRemoteDescription(remoteDescription);
-	}
-
-	async sendOffer() {
-		const offer = await this.createOffer();
-		const res = await fetch("/api/room/offer", {
-			method: "post",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({ offer, roomId: this.id }),
-		});
-
-		if (res.ok) {
-			console.log("Offer sent successfully");
-		} else {
-			console.error("Failed to send offer");
+	async handleWSMessage(e) {
+		if (typeof e.data !== "string") return;
+		const data = JSON.parse(e.data);
+		if (Array.isArray(data.users)) {
+			// After first joining the room, an array of current users in the room is sent
+			for (const user of data.users) {
+				if (typeof user !== "string" || user === UID || this.peers.has(user))
+					continue;
+				const peer = new RoomPeerConnection(user, this.ws);
+				this.peers.set(user, peer);
+				peer.offer();
+			}
+		} else if (
+			typeof data.from === "string" &&
+			!this.peers.has(data.from) &&
+			data.from !== UID
+		) {
+			// If a message is sent from a user that is not a current peer, then they should be sending an offer
+			// else send an offer back
+			/**
+			 * @type {ConstructorParameters<typeof RoomPeerConnection>}
+			 */
+			const constructorParams = [data.from, this.ws];
+			if (data.message?.type === "offer") {
+				constructorParams.push(data.message);
+			}
+			const peer = new RoomPeerConnection(...constructorParams);
+			this.peers.set(data.from, peer);
 		}
 	}
 }

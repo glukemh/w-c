c5bb58b9eb6f6157c51d4137eaa8e73905c4c9b2
Luke Mendelman-Haenn <glukemh@gmail.com>
2023-12-04 00:02:42 -0700

    fade in with adopted stylesheet

--- public/components/main-page.js
+++ public/components/main-page.js
@@ -1,8 +1,17 @@
 import "/components/main-layout.js";
 import "/components/main-nav.js";
 import mixinForShadowContent from "/assets/mixin-for-shadow-content.js";
+import fetchStylesheet from "/assets/fetch-stylesheet.js";
 
-const mixin = await mixinForShadowContent("main-page");
-export default class MainPage extends mixin(HTMLElement) {}
+const [mixin, stylesheet] = await Promise.all([
+	mixinForShadowContent("main-page"),
+	fetchStylesheet("host-fade-in"),
+]);
+export default class MainPage extends mixin(HTMLElement) {
+	constructor() {
+		super();
+		this.shadow.adoptedStyleSheets = [stylesheet];
+	}
+}
 
 customElements.define("main-page", MainPage);

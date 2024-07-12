2cc51eb81219fd2fbd9eceb410aa804ed959d191
GLukeM-H <glukemh@gmail.com>
2024-07-12 00:32:13 -0500

    submit using form action

---
+++ public/components/submit-to-history.js
@@ -0,0 +1,43 @@
+export default class SubmitToHistory extends HTMLElement {
+	static get observedAttributes() {
+		return ["submit-params"];
+	}
+	static formAssociated = true;
+	/** @param {SubmitEvent} e */
+	#onFormSubmit = (e) => {
+		e.preventDefault();
+		const { form } = this.#internals;
+		if (!form) return;
+		const formData = new FormData(form);
+		const url = new URL(form.action);
+		switch (this.getAttribute("submit-params")) {
+			case "replace":
+				url.search = "";
+				break;
+			case "append":
+			default:
+				break;
+		}
+		for (const [key, value] of formData) {
+			if (typeof value === "string") {
+				url.searchParams.append(key, value);
+			}
+		}
+		history.replaceState({}, "", url);
+		if (url.hash) {
+			location.hash = url.hash;
+		}
+	};
+	#internals = this.attachInternals();
+
+	/** @param {HTMLFormElement} form */
+	formAssociatedCallback(form) {
+		if (this.#internals.form) {
+			form.addEventListener("submit", this.#onFormSubmit);
+		} else {
+			form.removeEventListener("submit", this.#onFormSubmit);
+		}
+	}
+}
+
+customElements.define("submit-to-history", SubmitToHistory);

3fae4b7372294094ebe62eb3ef78d100ad8f688c
Luke Mendelman-Haenn <glukemh@gmail.com>
2024-11-23 17:49:11 -0600

    slot list utility component

---
+++ public/components/slot-list.js
@@ -0,0 +1,23 @@
+export default class SlotList extends HTMLElement {
+	#shadow = this.attachShadow({ mode: "open" });
+	constructor() {
+		super();
+		this.#newSlot();
+	}
+
+	#newSlot() {
+		const slot = document.createElement("slot");
+		slot.name = this.#shadow.children.length.toString();
+		slot.addEventListener("slotchange", this);
+		this.#shadow.append(slot);
+	}
+
+	/** @param {Event} e */
+	handleEvent(e) {
+		const { target } = e;
+		if (!(target instanceof HTMLSlotElement)) return;
+		if (!target.assignedNodes().length) return;
+		target.removeEventListener("slotchange", this);
+		this.#newSlot();
+	}
+}

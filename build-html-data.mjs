525498a4b8b1d8fc449d2951630c630c8ba1b702
Luke Mendelman-Haenn <glukemh@gmail.com>
2024-11-25 09:45:55 -0700

    build html data code organization

--- build-html-data.mjs
+++ build-html-data.mjs
@@ -1,29 +1,35 @@
 import fs from "fs";
 import path from "path";
 
-const outFile = "html.html-data.json";
+const htmlDataExt = ".html-data.json";
+const outFile = "html" + htmlDataExt;
+const { dirname } = import.meta;
+const htmlDataFile = path.join(dirname, outFile);
+const componentsFolder = path.join(dirname, "public", "components");
 
-const allFiles = getAllFiles(
-	path.join(import.meta.dirname, "public", "components")
-);
-const htmlDataFiles = allFiles.filter((file) =>
-	file.endsWith(".html-data.json")
+const htmlDataFiles = getAllFiles(componentsFolder).filter((file) =>
+	file.endsWith(htmlDataExt)
 );
 const tags = tagData(htmlDataFiles);
 
-const htmlDataFile = path.join(import.meta.dirname, outFile);
-const htmlData = JSON.parse(fs.readFileSync(htmlDataFile, "utf8"));
+const htmlData = readJsonFile(htmlDataFile);
 htmlData.tags = tags;
-fs.writeFileSync(htmlDataFile, JSON.stringify(htmlData, null, "\t"));
+writeJsonFile(htmlDataFile, htmlData);
 console.log("HTMl data written to " + htmlDataFile);
 
 /** @param {string[]} files */
 function tagData(files) {
 	const tags = [];
 	for (const file of files) {
-		const htmlData = JSON.parse(fs.readFileSync(file, "utf8"));
-		if ("tags" in htmlData && Array.isArray(htmlData.tags)) {
-			tags.push(...htmlData.tags);
+		try {
+			const htmlData = readJsonFile(file);
+			if ("tags" in htmlData && Array.isArray(htmlData.tags)) {
+				tags.push(...htmlData.tags);
+			} else {
+				throw "No tags property found in " + file;
+			}
+		} catch (e) {
+			console.error("Error occurred reading file " + file, e);
 		}
 	}
 	return tags;
@@ -44,4 +50,16 @@ function getAllFiles(dirPath, arrayOfFiles = []) {
 	});
 
 	return arrayOfFiles;
+}
+
+/** @param {string} filePath */
+function readJsonFile(filePath) {
+	return JSON.parse(fs.readFileSync(filePath, "utf8"));
+}
+
+/**
+ * @param {string} filePath
+ * @param {unknown} data */
+function writeJsonFile(filePath, data) {
+	fs.writeFileSync(filePath, JSON.stringify(data, null, "\t"));
 }

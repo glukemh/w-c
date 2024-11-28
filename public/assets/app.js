60309c503539fc615277fbb0548e4c2d3f4d2446
Luke Mendelman-Haenn <glukemh@gmail.com>
2024-11-28 15:27:03 -0700

    module app.js

--- public/assets/app.js
+++ public/assets/app.js
@@ -1,14 +1,14 @@
-app();
-async function app() {
-	try {
-		const registration = await navigator.serviceWorker.register(
-			"/service-worker.js"
-		);
-		console.log(
-			"ServiceWorker registration successful with scope: ",
-			registration.scope
-		);
-	} catch (error) {
-		console.error("ServiceWorker registration failed: ", error);
-	}
+try {
+	const registration = await navigator.serviceWorker.register(
+		"/service-worker.js",
+		{ type: "module" }
+	);
+	console.log(
+		"ServiceWorker registration successful with scope: ",
+		registration.scope
+	);
+} catch (error) {
+	console.error("ServiceWorker registration failed: ", error);
 }
+
+export {};

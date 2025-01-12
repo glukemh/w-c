e376c277877dfa229c6333c6cf544a2baa7b88e6
GLukeM-H <glukemh@gmail.com>
2025-01-12 17:12:57 -0600

    remove old files

--- public/components/main-layout.js
+++
@@ -1,6 +0,0 @@
-import mixinForShadowContent from "/assets/mixin-for-shadow-content.js";
-
-const mixin = await mixinForShadowContent("main-layout");
-export default class MainLayout extends mixin(HTMLElement) {}
-
-customElements.define("main-layout", MainLayout);
--- public/components/main-nav.js
+++
@@ -1,7 +0,0 @@
-import "/components/a-route.js";
-import mixinForShadowContent from "/assets/mixin-for-shadow-content.js";
-
-const mixin = await mixinForShadowContent("main-nav");
-export default class MainNav extends mixin(HTMLElement) {}
-
-customElements.define("main-nav", MainNav);

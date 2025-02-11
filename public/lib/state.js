659dd7c57d2eb391f3c98ed8ca26c6d9767b7ad3
Luke Mendelman-Haenn <glukemh@gmail.com>
2025-02-11 13:56:25 -0600

    generator state

--- public/lib/state.js
+++ public/lib/state.js
@@ -1,105 +1,64 @@
-/** @template T */
-export default class State {
-  #closeController = new AbortController();
-  get closed() {
-    return this.#closeController.signal.aborted;
-  }
-
-  /** @type {[T] | []} */
-  #current = [];
-  /** @type {PromiseWithResolvers<boolean>} */
-  #next = Promise.withResolvers();
-
-  #setIt = this.#set();
-  get set() {
-    return this.#setIt;
-  }
-
-  #updateIt = this.#update();
-  get update() {
-    return this.#updateIt;
-  }
-
-  constructor() {
-    this.#setIt.next();
-    this.#updateIt.next();
-    this.#closeController.signal.addEventListener("abort", () => {
-      this.#current.splice(0);
-      this.#next.resolve(false);
-      this.#setIt.return();
-      this.#updateIt.return();
-    });
-  }
-
-  /** @returns {Generator<undefined, void, T>} */
-  * #set() {
-    try {
-      while (true) {
-        this.#current[0] = yield;
-        this.#next.resolve(true);
-        this.#next = Promise.withResolvers();
-      }
-    } finally {
-      this.#closeController.abort();
-    }
-  }
-
-  /** @returns {Generator<undefined, void, (value: T) => T>} */
-  * #update() {
-    try {
-      while (true) {
-        const f = yield;
-        if (this.#current.length) this.set.next(f(this.#current[0]));
-      }
-    } finally {
-      this.#closeController.abort();
-    }
-  }
-
-  /** @param {AsyncGenerator<T, void, void>} iter */
-  async source(iter) {
-    try {
-      if (this.closed) {
-        iter.return();
-      } else {
-        this.#closeController.signal.addEventListener("abort", () => iter.return());
-      }
-      for await (const value of iter) {
-        this.set.next(value);
-      }
-    } finally {
-      this.#closeController.abort();
-    }
-  }
-
-  /** @param {(iter: AsyncGenerator<T, void, void>) =>  void } [callback] */
-  subscribe(callback) {
-    const controller = new AbortController();
-    /** @type {Promise<false>} */
-    const finish = new Promise((resolve) => {
-      controller.signal.onabort = () => resolve(false);
-    });
-    const iter = this.#subscribe(finish);
-    /** @type {typeof iter['return']} */
-    const returnIter = iter.return.bind(iter);
-    iter.return = () => {
-      controller.abort();
-      return returnIter();
+/**
+ * Initiate state from a source. Returning from source will close the state. Subscribe to state changes by calling next.
+ * @template T
+ * @param {AsyncGenerator<T, void>} source source to update state
+ * @example
+ * ```js
+ * const subscribe = state(sourceGenerator);
+ * for await (const n of subscribe.next().value) {
+ *   console.log(n);
+ * }
+ */
+export default function* state(source) {
+  try {
+    const instance = {
+      /** @type {[T] | []} */
+      value: [],
+      /** @type {PromiseWithResolvers<boolean>} */
+      next: Promise.withResolvers(),
+      open: true,
+      source,
     };
-    callback?.(iter);
-    return iter;
-  }
-
-  [Symbol.asyncIterator]() {
-    return this.subscribe();
-  }
-
-  /** @param {Promise<false>} finish */
-  async * #subscribe(finish) {
-    let p;
-    do {
-      p = this.#next.promise;
-      yield* this.#current;
-    } while (await Promise.race([p, finish]));
+    handleSource(instance);
+    while (open) {
+      const returned = Promise.withResolvers();
+      const iter = subscribe(instance, returned.promise);
+      /** @type {typeof iter['return']} */
+      const returnIter = iter.return.bind(iter);
+      iter.return = () => {
+        returned.resolve(false);
+        return returnIter();
+      };
+      yield iter;
+    }
+    return { async *[Symbol.asyncIterator]() { } };
+  } finally {
+    source.return();
   }
 }
+
+/** @template T @param {Instance<T>} x @param {Promise<false>} returned */
+async function* subscribe(x, returned) {
+  let p;
+  do {
+    p = x.next.promise;
+    yield* x.value;
+  } while (await Promise.race([p, returned]));
+}
+
+/** @template T @param {Instance<T>} x */
+async function handleSource(x) {
+  for await (const v of x.source) {
+    x.value[0] = v;
+    x.next.resolve(true);
+    x.next = Promise.withResolvers();
+  }
+  x.open = false;
+  x.value.splice(0);
+  x.next.resolve(false);
+}
+
+/** 
+ * @template T
+ * @typedef {{ value: [T] | [], next: PromiseWithResolvers<boolean>, open: boolean, source: AsyncGenerator<T> }} Instance
+ */
